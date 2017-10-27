cd6d9d7b67c1d276a49debcb9e3b99c17ac91913
Griffin P Longtin <griflongtin@live.com>
2017-10-27 10:25:33 -0700

    add a javascript function that replayses any number that when / 3 that has a % 0 is replaysed

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,22 @@
-function translator(numbers) {
+function translator(number) {
   var numberArray = [];
-  for (var i = 0; i <= number; i++) {
-    numberArray.push(i);
+  for (var i = 1; i <= number; i++) {
+    if (i % 3 === 0) {
+      numberArray.push(" " + "I'm sorry, Dave. I'm afraid I can't do that.")
+    } else {
+      numberArray.push(" " + i);
+    }
 
     }
     return numberArray
-  }
 }
 
 
 
 
-
 $(function() {
-  
+  $("form").click(function(event) {
+    event.preventDefault();
+    $("#output").text(translator(parseInt($("#userinput").val())));
+  });
 });
