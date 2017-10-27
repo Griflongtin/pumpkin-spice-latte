71b81b7b142b18a1a3a622f6263d6b0c94af391c
Griffin P Longtin <griflongtin@live.com>
2017-10-27 09:13:57 -0700

    add a logic function that makes an array for number from 1 to input number

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+function translator(numbers) {
+  var numberArray = [];
+  for (var i = 0; i <= number; i++) {
+    numberArray.push(i);
+
+    }
+    return numberArray
+  }
+}
+
+
+
+
+
+$(function() {
+  
+});
