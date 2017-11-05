818aa092fc871184498f47c1bbf8abb5f72fc6eb
Griffin P Longtin <griflongtin@live.com>
2017-11-05 13:43:26 -0800

    add logic for my third spec

--- js/scripts.js
+++ js/scripts.js
@@ -3,8 +3,10 @@ function userInput(inputNumber){
   for (var i = 0; i <= inputNumber; i++) {
     if (i === 0) {
       latteArray.push(i);
+    } else if (i % 12 === 0) {
+      latteArray.push("latte");
     } else if (i % 4 === 0) {
-      latteArray.push("spice")
+      latteArray.push("spice");
     } else if (i % 3 === 0) {
       latteArray.push("pumpkin");
     } else {
